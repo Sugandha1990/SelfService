379b32b3e7e928c0c3495a31ee906f4c09dafd12
Kumar Atul <kumar.atul@intelligrape.com>
2015-06-29 16:00:08 +0530

    Config files

--- grails-app/domain/selfservice/Room.groovy
+++ grails-app/domain/selfservice/Room.groovy
@@ -4,11 +4,13 @@ import enums.RESOURCE_TYPE
 import enums.ROOM_TYPE
 
 class Room {
+
+
     String name
     Integer floor
     Integer capacity
     ROOM_TYPE  roomType
-    List<RESOURCE_TYPE>
+    List<RESOURCE_TYPE> resources
     static constraints = {
         floor validator: {val,obj->
             if(Room.findByFloorAndName(val,obj.name))
